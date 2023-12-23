e854ec43ff9fb82809c3afeca7b88705c47e4ac0
Satyadev Moolagani <smoolagani@gmail.com>
2023-12-23 06:11:49 -0500

    Function Wrapper + UX

--- ytdownloader.py
+++ ytdownloader.py
@@ -2,20 +2,30 @@ from pytube import YouTube
 
 SAVE_PATH = "/Users/satyadev/Desktop/Python Projects/Scripts/videos"
 
-link= str(input("Link of the YouTube Video: "))
+def download_video(yt_video_link):
+
+	try:
+		yt = YouTube(yt_video_link)
+	except:
+		print("Connection Error")
+		return
+		
+	mp4_files = yt.streams.filter(file_extension='mp4')
+	highest_res_vid = mp4_files.get_highest_resolution()
+
+	try:
+		highest_res_vid.download(SAVE_PATH)
+	except Exception as e:
+		print(e)
+		return
+		
+	print('Task Completed!')
 
 try:
-	yt = YouTube(link)
-except:
-	print("Connection Error")
-	
-mp4_files = yt.streams.filter(file_extension='mp4')
-highest_res_vid = mp4_files.get_highest_resolution()
-
-try:
-	highest_res_vid.download(SAVE_PATH)
-except Exception as e:
-	print(e)
-	
-print('Task Completed!')
+	while True:
+		get_yt_video_link = str(input("Enter YouTube Video Link: "))
+		download_video(get_yt_video_link)
+        
+except KeyboardInterrupt:
+    print('Process Completed!')
 
